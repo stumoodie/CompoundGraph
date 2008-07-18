2d097378cdf7343763bacd55e69d788f8f8ca89b
Stuart Moodie <stuart.moodie@eightpillars.uk.com>
2008-07-18 15:01:55 +0000

    Reinstating this class.

---
+++ test/uk/ed/inf/graph/compound/impl/CompoundGraphTest.java
@@ -0,0 +1,194 @@
+package uk.ed.inf.graph.compound.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompoundGraphTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testCompoundGraph() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCompoundGraphCompoundGraph() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetRoot() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsDirectedEdgeCompoundNodeCompoundNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCanCreateEdges() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCanCreateNodes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCanCreateSubgraphs() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCanRemoveSubgraphs() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsConnectionCompoundNodeCompoundNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsEdgeCompoundEdge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsEdgeInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsNodeInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsNodeCompoundNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEdgeFactory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNodeFactory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSubgraphFactory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetEdge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEdgeIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNodeIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetNumEdges() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetNumNodes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRemoveSubgraph() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetLcaNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetNodeCounter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetEdgeCounter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsDirectedEdgeIDirectedPairOfCompoundNodeCompoundEdge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testContainsConnectionIBasicPairOfCompoundNodeCompoundEdge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCurrentState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRestoreState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCanCopyHere() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCopyHere() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCreateCopy() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testClear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCopiedComponents() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
