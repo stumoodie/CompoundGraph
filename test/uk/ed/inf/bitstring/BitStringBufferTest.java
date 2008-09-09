895b195997062b16085851ae4767514ff7ae771d
Nicos Tsorman <ntsorman@staffmail.ed.ac.uk>
2008-09-09 10:51:17 +0000

    BitStringTest

--- test/uk/ed/inf/bitstring/BitStringBufferTest.java
+++ test/uk/ed/inf/bitstring/BitStringBufferTest.java
@@ -2,14 +2,28 @@ package uk.ed.inf.bitstring;
 
 import static org.junit.Assert.*;
 
+import java.util.BitSet;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BitStringBufferTest {
+	
+	private BitStringBuffer testBitStringBuffer ;
+	private BitStringBuffer testBitStringBuffer2 ;
+	
+	public static final int [] NUMERICAL = {0,1,2,3,4,5 , 64} ;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		testBitStringBuffer = new BitStringBuffer (3) ;
+		testBitStringBuffer.flip(1) ;
+		
+		testBitStringBuffer2 = new BitStringBuffer (3) ;
+		testBitStringBuffer2.flip(0) ;
+		testBitStringBuffer2.flip(1) ;
 	}
 
 	@After
@@ -18,147 +32,281 @@ public class BitStringBufferTest {
 
 	@Test
 	public final void testHashCode() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testBitStringBuffer() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testBitStringBufferInt() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "same object" , testBitStringBuffer.hashCode() , testBitStringBuffer.hashCode()) ;
+		assertNotSame ( "not same object" , testBitStringBuffer.hashCode() , testBitStringBuffer2.hashCode()) ;
 	}
 
 	@Test
 	public final void testBitStringBufferIBitString() {
-		fail("Not yet implemented"); // TODO
+		IBitString aBitString = new BitString ( 3 ) ; 
+		testBitStringBuffer = new BitStringBuffer ( aBitString ) ;
+		
+		
+		assertEquals ( "from BitString" , aBitString.get(NUMERICAL[0]) , testBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "from BitString" , aBitString.get(NUMERICAL[1]) , testBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "from BitString" , aBitString.get(NUMERICAL[2]) , testBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testAnd() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (NUMERICAL[3]) ;
+		result.flip(NUMERICAL[1]) ;
+		
+		testBitStringBuffer2.and(testBitStringBuffer) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testAndNot() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		
+		testBitStringBuffer2.andNot(testBitStringBuffer) ;
+		
+		assertEquals ( "andNot operator" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "andNot operator" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "andNot operator" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testCardinality() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ("2 true" , NUMERICAL[2] , testBitStringBuffer2.cardinality()) ;
+		assertEquals ("1 true" , NUMERICAL[1] , testBitStringBuffer.cardinality()) ;
 	}
 
 	@Test
 	public final void testClear() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;		
+		testBitStringBuffer.clear() ;
+		
+		assertEquals ( "clear" , result.get(NUMERICAL[0]) , testBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "clear" , result.get(NUMERICAL[1]) , testBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "clear" , result.get(NUMERICAL[2]) , testBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testClearIntInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(NUMERICAL[0]) ;
+		
+		testBitStringBuffer2.clear( 1 , 2 ) ;
+		
+		assertEquals ( "clear int int" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "clear int int" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "clear int int" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testClearInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(NUMERICAL[1]) ;
+		
+		testBitStringBuffer2.clear( 0 ) ;
+		
+		assertEquals ( "clear int" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "clear int" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "clear int" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testEqualsObject() {
-		fail("Not yet implemented"); // TODO
+		BitStringBuffer aBitSTringBuffer = new BitStringBuffer ( 3 ) ;
+		aBitSTringBuffer.flip(1) ;
+		
+		
+		assertEquals ( "same Object " , aBitSTringBuffer , testBitStringBuffer ) ;
 	}
 
 	@Test
 	public final void testFlipIntInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(NUMERICAL[2]) ;
+		
+		testBitStringBuffer.flip(1, 3) ;
+		
+		assertEquals ( "flip int int" , result.get(NUMERICAL[0]) , testBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "flip int int" , result.get(NUMERICAL[1]) , testBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "flip int int" , result.get(NUMERICAL[2]) , testBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testFlipInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		
+		testBitStringBuffer.flip(1) ;
+		
+		assertEquals ( "flip" , result.get(NUMERICAL[0]) , testBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "flip" , result.get(NUMERICAL[1]) , testBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "flip" , result.get(NUMERICAL[2]) , testBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testGetIntInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(NUMERICAL[0]) ;
+		result.flip(NUMERICAL[1]) ;
+		
+		IBitString other = testBitStringBuffer2.get( 0 , 3) ;
+		
+		assertEquals ( "get" , result.get(NUMERICAL[0]) , other.get(NUMERICAL[0])) ;
+		assertEquals ( "get" , result.get(NUMERICAL[1]) , other.get(NUMERICAL[1])) ;
+		assertEquals ( "get" , result.get(NUMERICAL[2]) , other.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testGetInt() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "get" , true , testBitStringBuffer.get(NUMERICAL[1])) ;
 	}
 
 	@Test
 	public final void testIntersects() {
-		fail("Not yet implemented"); // TODO
+		assertTrue ( "intersects" , testBitStringBuffer.intersects(testBitStringBuffer2));
+		
+		BitStringBuffer result = new BitStringBuffer (3) ;
+		result.flip(NUMERICAL[0]) ;
+		result.flip(NUMERICAL[2]) ;
+		
+		assertFalse ( "intersects" , testBitStringBuffer.intersects(result));
 	}
 
 	@Test
 	public final void testIsEmpty() {
-		fail("Not yet implemented"); // TODO
+		assertFalse ( "notEmpty" , testBitStringBuffer.isEmpty()) ;
+		testBitStringBuffer.clear() ;
+		assertTrue ( "Empty" , testBitStringBuffer.isEmpty()) ;
+		
 	}
 
 	@Test
 	public final void testLength() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "length" , NUMERICAL[2] , testBitStringBuffer.length()) ;
+
+		BitStringBuffer other = new BitStringBuffer (NUMERICAL[3]) ;
+		other.flip(NUMERICAL[2]) ;
+		assertEquals ( "length of last true" , NUMERICAL[3] , other.length()) ;
+		
+		other = new BitStringBuffer ( 3 ) ;
+		assertEquals ( "nothing in" , NUMERICAL[0] , other.length()) ;
 	}
 
 	@Test
 	public final void testNextClearBit() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("fist index" , NUMERICAL[0] , testBitStringBuffer.nextClearBit(NUMERICAL[0])) ;
+		assertEquals("last index" , NUMERICAL[2] , testBitStringBuffer2.nextClearBit(NUMERICAL[0])) ;
 	}
 
 	@Test
 	public final void testNextSetBit() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("second index" , NUMERICAL[1] , testBitStringBuffer.nextSetBit(NUMERICAL[0])) ;
+		assertEquals("first index" , NUMERICAL[0] , testBitStringBuffer2.nextSetBit(NUMERICAL[0])) ;
 	}
 
 	@Test
 	public final void testOr() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		
+		testBitStringBuffer2.or(testBitStringBuffer) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testSetIntBoolean() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		result.flip(2) ;
+		
+		testBitStringBuffer2.set(2, true) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testSetIntIntBoolean() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		result.flip(2) ;
+		
+		testBitStringBuffer.set(0, 3 , true) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testSetIntInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		result.flip(2) ;
+		
+		testBitStringBuffer.set(0, 3 ) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testSetInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		result.flip(2) ;
+		
+		testBitStringBuffer2.set(2 ) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testSize() {
-		fail("Not yet implemented"); // TODO
+		assertEquals( "size" , NUMERICAL[6] , testBitStringBuffer.size() ) ;
 	}
 
 	@Test
 	public final void testToString() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "single bit" , "{1}" , testBitStringBuffer.toString()) ;
+		assertEquals ( "two bit" , "{0, 1}" , testBitStringBuffer2.toString()) ;
 	}
 
 	@Test
 	public final void testToBitString() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		
+		IBitString fromBitStringBuffer = testBitStringBuffer2.toBitString() ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , fromBitStringBuffer.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , fromBitStringBuffer.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , fromBitStringBuffer.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testXor() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		
+		testBitStringBuffer2.xor(testBitStringBuffer) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , testBitStringBuffer2.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , testBitStringBuffer2.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , testBitStringBuffer2.get(NUMERICAL[2])) ;
 	}
 
 }
--- test/uk/ed/inf/bitstring/BitStringTest.java
+++ test/uk/ed/inf/bitstring/BitStringTest.java
@@ -2,14 +2,35 @@ package uk.ed.inf.bitstring;
 
 import static org.junit.Assert.*;
 
+import java.util.BitSet;
+import java.lang.reflect.Array;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.org.apache.xerces.internal.util.TeeXMLDocumentFilterImpl;
+
 public class BitStringTest {
+	
+	private BitString testBitString ;
+	private BitString testBitString2 ;
+	
+	public static final int [] NUMERICAL = {0,1,2,3,4,5 , 64} ;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		BitSet tempSet = new BitSet (NUMERICAL[3]) ;
+		tempSet.flip(NUMERICAL[1]) ;
+		
+		testBitString = new BitString( tempSet) ;
+		
+		tempSet = new BitSet (NUMERICAL[3]) ;
+		tempSet.flip(NUMERICAL[0]) ;
+		tempSet.flip(NUMERICAL[1]) ;
+		
+		testBitString2 = new BitString ( tempSet ) ;
 	}
 
 	@After
@@ -18,112 +39,183 @@ public class BitStringTest {
 
 	@Test
 	public final void testHashCode() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testBitString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testBitStringInt() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "hashcode" , testBitString.hashCode() , testBitString.hashCode() ) ;
 	}
 
 	@Test
 	public final void testBitStringBitString() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (NUMERICAL[3]) ;
+		result.flip(NUMERICAL[1]) ;
+		
+		BitString newBitString = new BitString ( testBitString) ;
+		assertEquals ( "new from BitString" , result.get(NUMERICAL[0]) , testBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "new from BitString" , result.get(NUMERICAL[1]) , testBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "new from BitString" , result.get(NUMERICAL[2]) , testBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testBitStringBitSet() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (NUMERICAL[3]) ;
+		result.flip(NUMERICAL[1]) ;
+		
+		BitString newBitString = new BitString ( result) ;
+		assertEquals ( "new from BitSet" , result.get(NUMERICAL[0]) , testBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "new from BitSet" , result.get(NUMERICAL[1]) , testBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "new from BitSet" , result.get(NUMERICAL[2]) , testBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testAnd() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (NUMERICAL[3]) ;
+		result.flip(NUMERICAL[1]) ;
+		
+		IBitString andBitString = testBitString2.and(testBitString) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , andBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , andBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , andBitString.get(NUMERICAL[2])) ;
+		
 	}
 
 	@Test
 	public final void testAndNot() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		
+		IBitString andNotBitString = testBitString2.andNot(testBitString) ;
+		
+		assertEquals ( "andNot operator" , result.get(NUMERICAL[0]) , andNotBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "andNot operator" , result.get(NUMERICAL[1]) , andNotBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "andNot operator" , result.get(NUMERICAL[2]) , andNotBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testCardinality() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ("2 true" , NUMERICAL[2] , testBitString2.cardinality()) ;
+		assertEquals ("1 true" , NUMERICAL[1] , testBitString.cardinality()) ;
 	}
 
 	@Test
 	public final void testEqualsObject() {
-		fail("Not yet implemented"); // TODO
+		assertTrue ( "same object" , testBitString.equals(testBitString) );
+		assertFalse ( "other object" , testBitString.equals( testBitString2 ) );
 	}
 
 	@Test
 	public final void testGetIntInt() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(1) ;
+		
+		IBitString getBitString = testBitString.get(0, 2) ; 
+		
+		assertEquals ( "get int int" , result.get(NUMERICAL[0]) , getBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "get int int" , result.get(NUMERICAL[1]) , getBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "get int int" , result.get(NUMERICAL[2]) , getBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testGetInt() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "get int" , false , testBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "get int" , true , testBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "get int" , false , testBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testIntersects() {
-		fail("Not yet implemented"); // TODO
+		assertTrue ( "intersects" , testBitString.intersects(testBitString2)) ;
 	}
 
 	@Test
 	public final void testIsEmpty() {
-		fail("Not yet implemented"); // TODO
+		assertFalse ( "notEmpty" , testBitString.isEmpty()) ;
 	}
 
 	@Test
 	public final void testLength() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "length" , NUMERICAL[2] , testBitString.length()) ;
+		
+		BitSet aBitSet = new BitSet ( 3 ) ;
+		aBitSet.flip(2);
+		IBitString other = new BitString (aBitSet) ;
+		assertEquals ( "length of last true" , NUMERICAL[3] , other.length()) ;
+		
+		aBitSet = new BitSet ( 3 ) ;
+		other = new BitString (aBitSet) ;
+		assertEquals ( "nothing in" , NUMERICAL[0] , other.length()) ;
+		
 	}
 
 	@Test
 	public final void testNextClearBit() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("fist index" , NUMERICAL[0] , testBitString.nextClearBit(NUMERICAL[0])) ;
+		assertEquals("last index" , NUMERICAL[2] , testBitString2.nextClearBit(NUMERICAL[0])) ;
 	}
 
 	@Test
 	public final void testNextSetBit() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("second index" , NUMERICAL[1] , testBitString.nextSetBit(NUMERICAL[0])) ;
+		assertEquals("first index" , NUMERICAL[0] , testBitString2.nextSetBit(NUMERICAL[0])) ;
 	}
 
 	@Test
 	public final void testOr() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		result.flip(1) ;
+		
+		IBitString orBitString = testBitString2.or(testBitString) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , orBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , orBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , orBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testSize() {
-		fail("Not yet implemented"); // TODO
+		assertEquals( "size" , NUMERICAL[6] , testBitString.size() ) ;
 	}
 
 	@Test
 	public final void testToString() {
-		fail("Not yet implemented"); // TODO
+		assertEquals ( "single bit" , "{1}" , testBitString.toString()) ;
+		assertEquals ( "two bit" , "{0, 1}" , testBitString2.toString()) ;
 	}
 
 	@Test
 	public final void testXor() {
-		fail("Not yet implemented"); // TODO
+		BitSet result = new BitSet (3) ;
+		result.flip(0) ;
+		
+		IBitString xorBitString = testBitString2.xor(testBitString) ;
+		
+		assertEquals ( "and operator" , result.get(NUMERICAL[0]) , xorBitString.get(NUMERICAL[0])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[1]) , xorBitString.get(NUMERICAL[1])) ;
+		assertEquals ( "and operator" , result.get(NUMERICAL[2]) , xorBitString.get(NUMERICAL[2])) ;
 	}
 
 	@Test
 	public final void testToArray() {
-		fail("Not yet implemented"); // TODO
+		boolean boolArray [] = {false , true , false } ;
+		
+		boolean generatedArray [] = testBitString.toArray() ;
+		
+		for ( int a = 0 ; a < Array.getLength(generatedArray) ; a++)
+		{
+			assertEquals ( "same" , boolArray[a] , generatedArray[a]) ;
+		}
 	}
 
 	@Test
 	public final void testToArrayBooleanArray() {
-		fail("Not yet implemented"); // TODO
+		boolean boolArray [] = {false , true , false } ;
+		boolean generatedArray [] = {true } ;
+		
+		 generatedArray = testBitString.toArray(generatedArray) ;
+		 
+			for ( int a = 0 ; a < Array.getLength(generatedArray) ; a++)
+			{
+				assertEquals ( "same" , boolArray[a] , generatedArray[a]) ;
+			}
 	}
 
 }
