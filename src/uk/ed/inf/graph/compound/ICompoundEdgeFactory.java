a8285b9f1bb6ebd5b2e9302a53d2fdd6891d4644
Stuart Moodie <stuart.moodie@eightpillars.uk.com>
2008-09-12 18:26:13 +0000

    Removed methods implemented by super class

--- src/uk/ed/inf/graph/compound/ICompoundEdgeFactory.java
+++ src/uk/ed/inf/graph/compound/ICompoundEdgeFactory.java
@@ -17,33 +17,6 @@ public interface ICompoundEdgeFactory<
 		E extends ICompoundEdge<N, E>
 > extends IDirectedEdgeFactory<N, E> {
 
-	/**
-	 * Sets the nodes to be used to create the edge.
-	 * @param outNode
-	 * @param inNode
-	 * @throws IllegalArgumentException if the nodes are not valid, ie. <code>isValidNodePair(outNode, inNode) == true</code>.
-	 */
-	void setPair(N outNode, N inNode);
-	
-	/**
-	 * Tests if the LCA of <code>outNode</code> and <code>inNode</code> is the root node of the owning child graph.
-	 * @param outNode out node of edge, can be null.
-	 * @param inNode in node of edge, can be null.
-	 * @return true if this is the case, false otherwise. 
-	 */
-	boolean isValidNodePair(N outNode, N inNode);
-	
-	/**
-	 * The child graph that will contain any edges created by this factory. 
-	 * @return The child graph, cannot be null.
-	 */
-	IChildCompoundGraph<N, E> getOwningChildGraph();
-	
-	/**
-	 * Creates a new directed edge from <code>outNode</code> to <code>inNode</code> which is
-	 *  owned by the childCompoundGraph returned by <code>getOwningChildGraph()</code>.
-	 * @return The newly created edge.
-	 * @throw IllegalArgumentException if <code>isValidNodePair(outNode, inNode) == false</code>.  
-	 */
-	E createEdge();
+	ICompoundGraph<N, E> getGraph();
+
 }
